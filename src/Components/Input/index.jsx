911f5e6dc9b205e9c615c0da779168cd902db0fe
FredericoGrz <frederico.graz@gmail.com>
2023-12-30 09:54:48 -0300

    Criado o componente de input

---
+++ src/Components/Input/index.jsx
@@ -0,0 +1,30 @@
+import Proptypes from "prop-types";
+
+export function Input({ icon: Icon, label, id, ...rest }) {
+  return (
+    <div className="flex flex-col gap-2">
+      {label && (
+        <label
+          htmlFor={id}
+          className="text-white"
+        >
+          {label}
+        </label>
+      )}
+      <div className="w-full flex items-center p-3 bg-dark-900 rounded-md">
+        {Icon && <Icon />}
+        <input
+          id={id}
+          className="bg-transparent w-full text-white outline-none"
+          {...rest}
+        />
+      </div>
+    </div>
+  );
+}
+
+Input.propTypes = {
+  icon: Proptypes.func,
+  label: Proptypes.string,
+  id: Proptypes.string,
+};
