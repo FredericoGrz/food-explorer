3e561ada836ae1e4898b2d1a159982ef6b14856c
FredericoGrz <frederico.graz@gmail.com>
2023-12-30 09:55:39 -0300

    criado a pagina de signup

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,58 @@
+import icon from "../assets/icon.svg";
+import { Input } from "../Components/Input";
+import { Button } from "../Components/Button";
+
+function SignIn() {
+  return (
+    <div className="min-w-full min-h-screen bg-dark-400 pt-36 px-12 lg:px-28">
+      <div
+        id="content"
+        className="flex flex-col lg:flex-row lg:justify-between"
+      >
+        <div
+          id="logo"
+          className="flex gap-2 items-center"
+        >
+          <img
+            src={icon}
+            alt="Logo do app"
+            className="w-11 h-11"
+          />
+          <h1 className="text-white text-4xl font-bold">Food Explorer</h1>
+        </div>
+        <form className="pt-16 flex flex-col gap-8 lg:p-16 lg:w-[475px] lg:bg-dark-700 lg:rounded-2xl">
+          <h2 className="hidden lg:block text-white text-3xl text-center font-medium">
+            Faça Login
+          </h2>
+          <Input
+            placeholder="Exemplo: Maria da Silva"
+            id="nome"
+            label="Seu Nome"
+            type="text"
+          />
+          <Input
+            placeholder="exemplo@exemplo.com.br"
+            id="email"
+            label="Email"
+            type="email"
+          />
+          <Input
+            placeholder="No mínimo 6 caracteres"
+            id="password"
+            label="Senha"
+            type="password"
+          />
+          <Button title="Criar conta" />
+          <a
+            href="#"
+            className="text-white text-center text-sm"
+          >
+            Já tenho uma conta
+          </a>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
