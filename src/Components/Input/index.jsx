8b4ca8388e622ae26f50db9acbc04f6a9dbd152a
FredericoGrz <frederico.graz@gmail.com>
2023-12-31 08:53:50 -0300

    Adicionado um gap entre o ícone e o input

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -11,8 +11,8 @@ export function Input({ icon: Icon, label, id, ...rest }) {
           {label}
         </label>
       )}
-      <div className="w-full flex items-center p-3 bg-dark-900 rounded-md">
-        {Icon && <Icon />}
+      <div className="w-full flex items-center p-3 gap-3 bg-dark-900 rounded-md">
+        {Icon && <Icon className="text-white text-xl" />}
         <input
           id={id}
           className="bg-transparent w-full text-white outline-none"
