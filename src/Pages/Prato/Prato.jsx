f9c00f53e1ea06aaf8f5c731e41635419b0e74a4
FredericoGrz <frederico.graz@gmail.com>
2024-01-05 12:24:02 -0300

    Inicializado a tela de adição de pratos

---
+++ src/Pages/Prato/Prato.jsx
@@ -0,0 +1,50 @@
+import { GrPrevious } from "react-icons/gr";
+import { Footer } from "../../Components/Footer";
+import { Header } from "../../Components/Header";
+import { InputFile } from "../../Components/InputFile";
+import { Input } from "../../Components/Input";
+import { InputSelect } from "../../Components/Select";
+
+function Prato() {
+  return (
+    <div className="h-screen w-full bg-dark-400">
+      <Header />
+      <div
+        id="container"
+        className="px-8 pt-2 pb-14 flex flex-col gap-8"
+      >
+        <div className="flex flex-col gap-6 text-light-300">
+          <button className="flex gap-1 items-center">
+            <GrPrevious />
+            Voltar
+          </button>
+          <h1 className="font-medium text-3xl">Novo Prato</h1>
+        </div>
+        <form className="flex flex-col gap-6">
+          <div className="flex flex-col gap-6">
+            <InputFile
+              id="imagem"
+              label="Imagem do prato"
+            />
+            <div className="lg:w-2/4">
+              <Input
+                id="nome"
+                label="Nome"
+                placeholder="Ex: Salada Ceasar"
+              />
+            </div>
+            <InputSelect
+              label="Categoria"
+              id="categoria"
+            />
+          </div>
+          <div></div>
+          <div></div>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Prato;
