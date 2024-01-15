96efba8376598290e1ea953292257cabd8e7d89e
FredericoGrz <frederico.graz@gmail.com>
2024-01-15 16:49:33 -0300

    Adicionado o ...rest para o componente de button

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-export function Button({ title, disabled = false, isLight = false }) {
+export function Button({ title, disabled = false, isLight = false, ...rest }) {
   return (
     <button
       type="button"
@@ -8,6 +8,7 @@ export function Button({ title, disabled = false, isLight = false }) {
       className={`w-full px-6 py-3 text-white rounded ${
         isLight ? "bg-tomato-400" : "bg-tomato-100"
       } hover:bg-tomato-200 disabled:bg-tomato-400 disabled:cursor-not-allowed`}
+      {...rest}
     >
       {title}
     </button>
