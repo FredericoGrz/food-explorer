860c86397d3df399bda85c75a5798b9ea3c3e8b0
FredericoGrz <frederico.graz@gmail.com>
2024-01-15 16:49:05 -0300

    Corrigido a mensagem quando havia um erro no login

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -17,7 +17,7 @@ function AuthProvider({ children }) {
       api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       setData({ user, token });
     } catch (error) {
-      alert(error.response);
+      alert(error.response.data.message);
     }
   }
 
@@ -62,60 +62,3 @@ function useAuth() {
 }
 
 export { AuthProvider, useAuth };
-
-// import { createContext, useContext, useState, useEffect } from "react";
-// import { api } from "../services/api";
-// import Proptypes from "prop-types";
-// const AuthContext = createContext({});
-
-// function AuthProvider({ children }) {
-//   const [data, setData] = useState({});
-
-//   async function signIn({ email, password }) {
-//     try {
-//       const response = await api.post("/sessions", { email, password });
-//       const { user, token } = response.data;
-//       localStorage.setItem("@foodexplorer:user", JSON.stringify(user));
-//       localStorage.setItem("@foodexplorer:token", token);
-
-//       api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-//       setData({ user, token });
-//     } catch (error) {
-//       alert(error.response);
-//     }
-//   }
-
-//   function signOut() {
-//     localStorage.removeItem("@foodexplorer:user");
-//     localStorage.removeItem("@foodexplorer:token");
-
-//     setData({});
-//   }
-
-//   useEffect(() => {
-//     const token = localStorage.getItem("@foodexplorer:token");
-//     const user = localStorage.getItem("@foodexplorer:user");
-//     if (token && user) {
-//       api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-//       setData({ token, user: JSON.parse(user) });
-//     }
-//   }, []);
-
-//   return (
-//     <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// }
-
-// AuthProvider.propTypes = {
-//   children: Proptypes.node,
-// };
-
-// function useAuth() {
-//   const context = useContext(AuthProvider);
-
-//   return context;
-// }
-
-// export { AuthProvider, useAuth };
