a3c0dad792cd15c9f6a2fdbf83614e0c37f6ada9
FredericoGrz <frederico.graz@gmail.com>
2024-01-16 11:16:09 -0300

    Criação de usuário integrado com o backend

--- src/Pages/SignUp/index.jsx
+++ src/Pages/SignUp/index.jsx
@@ -1,9 +1,34 @@
 import icon from "../../assets/icon.svg";
 import { Input } from "../../Components/Input";
 import { Button } from "../../Components/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAlertBox } from "../../hooks/AlertBox";
+import { api } from "../../services/api";
 
 function SignIn() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { showAlertBox } = useAlertBox();
+  const navigate = useNavigate();
+
+  async function handleSignUp() {
+    if (!name && !email && !password)
+      return showAlertBox({
+        message: "Preencha todos os campos",
+        type: "warning",
+      });
+    try {
+      const response = await api.post("/users", { name, email, password });
+      if (response.status === 201)
+        showAlertBox({ message: "Usuário criado com sucesso!" });
+      navigate("/");
+    } catch (error) {
+      showAlertBox({ message: error.response.data.message, type: "warning" });
+    }
+  }
+
   return (
     <div className="min-w-full min-h-screen bg-dark-400 pt-36 px-12 lg:px-28">
       <div
@@ -30,20 +55,29 @@ function SignIn() {
             id="nome"
             label="Seu Nome"
             type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <Input
             placeholder="exemplo@exemplo.com.br"
             id="email"
             label="Email"
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             placeholder="No mínimo 6 caracteres"
             id="password"
             label="Senha"
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <Button title="Criar conta" />
+          <Button
+            title="Criar conta"
+            onClick={handleSignUp}
+          />
           <Link
             to="/"
             className="text-white text-center text-sm"
