c3ec3ba8f1239bbc9f28b417063d536e17fab8a5
FredericoGrz <frederico.graz@gmail.com>
2024-01-16 10:37:31 -0300

    Hook de alertbox criado

---
+++ src/hooks/AlertBox.jsx
@@ -0,0 +1,61 @@
+import { createContext, useContext, useState } from "react";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { ImWarning } from "react-icons/im";
+import Proptypes from "prop-types";
+
+const AlertBoxContext = createContext({});
+
+function AlertBoxProvider({ children }) {
+  const [message, setMessage] = useState("");
+  const [alertType, setAlertType] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+
+  const iconMap = {
+    success: <BsFillCheckCircleFill className="text-light-300 text-xl" />,
+    warning: <ImWarning className="text-light-300 text-xl lg:text-2xl" />,
+  };
+
+  function renderIcon() {
+    if (alertType in iconMap) return iconMap[alertType];
+
+    return null;
+  }
+
+  function showAlertBox({ message = "", type = "success" }) {
+    setMessage(message);
+    setAlertType(type);
+    setIsVisible(true);
+
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 3000);
+  }
+
+  return (
+    <AlertBoxContext.Provider value={{ showAlertBox }}>
+      {children}
+      {isVisible && (
+        <div className="flex items-center p-2 rounded-xl gap-2 w-fit bg-cake-100 border-2 border-light-300 absolute z-40 top-8 right-0 left-0 mx-auto">
+          {renderIcon()}
+          {message && (
+            <span className="text-light-300 font-bold lg:text-xl">
+              {message}
+            </span>
+          )}
+        </div>
+      )}
+    </AlertBoxContext.Provider>
+  );
+}
+
+AlertBoxProvider.propTypes = {
+  children: Proptypes.node,
+};
+
+function useAlertBox() {
+  const context = useContext(AlertBoxContext);
+
+  return context;
+}
+
+export { AlertBoxProvider, useAlertBox };
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { AuthProvider } from "./hooks/auth";
 import { Routes } from "./routes";
 import "./index.css";
+import { AlertBoxProvider } from "./hooks/AlertBox";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    <AlertBoxProvider>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </AlertBoxProvider>
   </React.StrictMode>
 );
