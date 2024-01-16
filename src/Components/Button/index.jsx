e3e8061f081570e3f2f3abb78a45ac4480e84cce
FredericoGrz <frederico.graz@gmail.com>
2024-01-16 16:05:13 -0300

    Adicionado a opcao de "isLoading" no botao

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,15 +1,24 @@
 import PropTypes from "prop-types";
 
-export function Button({ title, disabled = false, isLight = false, ...rest }) {
+export function Button({
+  title,
+  disabled = false,
+  isLight = false,
+  isLoading,
+  ...rest
+}) {
   return (
     <button
       type="button"
       disabled={disabled}
-      className={`w-full px-6 py-3 text-white rounded ${
+      className={`w-full px-6 py-3 text-white rounded flex gap-2 items-center justify-center ${
         isLight ? "bg-tomato-400" : "bg-tomato-100"
       } hover:bg-tomato-200 disabled:bg-tomato-400 disabled:cursor-not-allowed`}
       {...rest}
     >
+      {isLoading && (
+        <div className="w-5 h-5 rounded-full border-2 border-light-300 border-t-white animate-spin"></div>
+      )}
       {title}
     </button>
   );
@@ -19,4 +28,5 @@ Button.propTypes = {
   title: PropTypes.string,
   disabled: PropTypes.bool,
   isLight: PropTypes.bool,
+  isLoading: PropTypes.bool,
 };
