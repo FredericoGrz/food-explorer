739c6480d9c938d767ba7fa15ce01471b765bebb
FredericoGrz <frederico.graz@gmail.com>
2024-01-17 10:59:10 -0300

    Parte de Incluir um novo prato concluído

--- src/Pages/Prato/Prato.jsx
+++ src/Pages/Prato/Prato.jsx
@@ -3,13 +3,79 @@ import { Footer } from "../../Components/Footer";
 import { Header } from "../../Components/Header";
 import { InputFile } from "../../Components/InputFile";
 import { Input } from "../../Components/Input";
-import { InputSelect } from "../../Components/Select";
 import { InputTag } from "../../Components/InputTag";
 import { InputPrice } from "../../Components/InputPrice";
+import { InputSelect } from "../../Components/Select";
 import { TextArea } from "../../Components/TextArea";
 import { Button } from "../../Components/Button";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { useAlertBox } from "../../hooks/AlertBox";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Prato() {
+  const { id } = useParams();
+  const isUpdate = id !== undefined;
+  const navigate = useNavigate();
+  const { showAlertBox } = useAlertBox();
+  const [imagem, setImagem] = useState("");
+  const [name, setName] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState();
+  const [ingredientes, setIngredientes] = useState([]);
+  const [preco, setPreco] = useState("");
+  const [description, setDescription] = useState("");
+
+  function verifyData() {
+    let allRequiredDataAvailable = true;
+    if (!imagem || !name || !category || !preco)
+      allRequiredDataAvailable = false;
+
+    return allRequiredDataAvailable;
+  }
+
+  async function handleSave() {
+    if (!verifyData())
+      return showAlertBox({
+        message: "Preencha todas as informações necessárias",
+        type: "warning",
+      });
+
+    try {
+      const response = await api.post("/pratos", {
+        imagem,
+        name,
+        description,
+        category_id: category,
+        ingredientes,
+        preco,
+      });
+
+      if (response.status === 201)
+        showAlertBox({
+          message: "Prato incluído com sucesso",
+          type: "success",
+        });
+
+      navigate("/");
+    } catch (error) {
+      showAlertBox({ message: error.response.data.message, type: "warning" });
+    }
+  }
+
+  useEffect(() => {
+    async function fetchCategoria() {
+      try {
+        const response = await api.get("categories");
+        setCategories(response.data);
+      } catch (error) {
+        showAlertBox({ message: error.response.data.message, type: "warning" });
+      }
+    }
+
+    fetchCategoria();
+  }, []);
+
   return (
     <div className="min-h-screen w-full bg-dark-400">
       <Header />
@@ -22,7 +88,9 @@ function Prato() {
             <GrPrevious className="lg:text-2xl" />
             <p className="lg:font-bold lg:text-2xl">Voltar</p>
           </button>
-          <h1 className="font-medium text-3xl">Novo Prato</h1>
+          <h1 className="font-medium text-3xl">
+            {isUpdate ? "Alterar Prato" : "Novo Prato"}
+          </h1>
         </div>
         <form className="flex flex-col gap-6 lg:gap-8">
           <div className="flex flex-col gap-6 lg:flex-row lg:gap-8">
@@ -30,6 +98,7 @@ function Prato() {
               <InputFile
                 id="imagem"
                 label="Imagem do prato"
+                onChange={(file) => setImagem(file)}
               />
             </div>
             <div className="lg:w-2/4">
@@ -37,12 +106,16 @@ function Prato() {
                 id="nome"
                 label="Nome"
                 placeholder="Ex: Salada Ceasar"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className="lg:w-1/4">
               <InputSelect
                 label="Categoria"
                 id="categoria"
+                options={categories}
+                onChange={(e) => setCategory(e.target.value)}
               />
             </div>
           </div>
@@ -51,12 +124,15 @@ function Prato() {
               <InputTag
                 label="Ingredientes"
                 id="ingrediente"
+                onChange={(tags) => setIngredientes(tags)}
               />
             </div>
             <div className="lg:w-1/4">
               <InputPrice
                 id="preco"
                 label="Preço"
+                value={preco}
+                onChange={(e) => setPreco(e.target.value)}
               />
             </div>
           </div>
@@ -65,12 +141,27 @@ function Prato() {
               label="Descrição"
               id="descricao"
               placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-          <div className="lg:w-1/5 lg:self-end">
+          <div
+            className={`${
+              isUpdate ? "lg:w-2/5" : "lg:w-1/5"
+            } lg:self-end flex gap-8`}
+          >
+            {isUpdate && (
+              <button
+                type="button"
+                className="w-full px-6 py-3 text-white rounded bg-dark-800"
+              >
+                Excluir Prato
+              </button>
+            )}
             <Button
-              title="Salvar Alterações"
+              title={isUpdate ? "Salvar Alterações" : "Criar Prato"}
               isLight
+              onClick={handleSave}
             />
           </div>
         </form>
