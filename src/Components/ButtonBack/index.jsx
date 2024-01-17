55939fe49674d44aaf6f7ccc6fbc327f356f6ecb
FredericoGrz <frederico.graz@gmail.com>
2024-01-17 11:13:31 -0300

    Adicionado a cor padrão do button back

--- src/Components/ButtonBack/index.jsx
+++ src/Components/ButtonBack/index.jsx
@@ -10,7 +10,7 @@ export function ButtonBack() {
   return (
     <button
       onClick={handleButtonBack}
-      className="w-fit flex gap-1 items-center hover:scale-125 transition-transform"
+      className="w-fit flex gap-1 items-center text-light-300 hover:scale-125 transition-transform"
     >
       <GrPrevious className="lg:text-2xl" />
       <p className="lg:font-bold lg:text-2xl">Voltar</p>
