1cba5093748596f476872d1b9712f828eb0a75db
FredericoGrz <frederico.graz@gmail.com>
2024-01-22 11:29:26 -0300

    Detalhes de prato integrado com backend

--- src/Pages/Prato/PratoDetalhes.jsx
+++ src/Pages/Prato/PratoDetalhes.jsx
@@ -1,10 +1,35 @@
-import ravanello from "../../assets/salada-ravanello.svg";
 import { Counter } from "../../Components/Counter";
 import { Button } from "../../Components/Button";
 import { Header } from "../../Components/Header";
 import { ButtonBack } from "../../Components/ButtonBack";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useAlertBox } from "../../hooks/AlertBox";
+import { api } from "../../services/api";
+import { Image } from "../../Components/Image";
 
 function PratoDetalhes() {
+  const { id } = useParams();
+  const { showAlertBox } = useAlertBox();
+  const [prato, setPrato] = useState({});
+
+  useEffect(() => {
+    async function fetchPrato() {
+      try {
+        const response = await api.get(`pratos/${id}`);
+
+        setPrato(response.data);
+      } catch (error) {
+        console.log(error);
+        showAlertBox({
+          message: "Nao foi possível carregar as informações do prato",
+          type: "warning",
+        });
+      }
+    }
+
+    fetchPrato();
+  }, []);
   return (
     <div className="min-w-full min-h-screen bg-dark-400">
       <Header />
@@ -15,11 +40,14 @@ function PratoDetalhes() {
         <div className="flex flex-col gap-4 lg:gap-10">
           <ButtonBack />
           <div className="self-center">
-            <img
-              src={ravanello}
-              alt="Salada Ravanello"
-              className="w-[264px] h-[264px] lg:min-w-[384px] lg:min-h-[384px]"
-            />
+            <div className="w-[264px] h-[264px] lg:min-w-[384px] lg:min-h-[384px]">
+              {prato.imagem && (
+                <Image
+                  imgUrl={prato.imagem}
+                  alt={`Imagem de ${prato.name}`}
+                />
+              )}
+            </div>
           </div>
         </div>
         <div>
@@ -28,33 +56,24 @@ function PratoDetalhes() {
             className="flex flex-col gap-6 items-center lg:items-start"
           >
             <h1 className="text-light-300 font-medium text-2xl lg:text-[40px]">
-              Salada Ravanello
+              {prato.name}
             </h1>
             <p className="text-light-300 line-clamp-2 text-center lg:text-start lg:text-2xl">
-              Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.
+              {prato.description}
             </p>
             <div
               id="tags"
               className="flex gap-6 flex-wrap lg:gap-3"
             >
-              <p className="bg-dark-1000 rounded text-white text-sm px-2 py-1">
-                Alface
-              </p>
-              <p className="bg-dark-1000 rounded text-white text-sm px-2 py-1">
-                Cebola
-              </p>
-              <p className="bg-dark-1000 rounded text-white text-sm px-2 py-1">
-                Pao Naan
-              </p>
-              <p className="bg-dark-1000 rounded text-white text-sm px-2 py-1">
-                Pepino
-              </p>
-              <p className="bg-dark-1000 rounded text-white text-sm px-2 py-1">
-                Rabanete
-              </p>
-              <p className="bg-dark-1000 rounded text-white text-sm px-2 py-1">
-                Tomate
-              </p>
+              {prato.ingredientes &&
+                prato.ingredientes.map((ingrediente, index) => (
+                  <p
+                    key={String(index)}
+                    className="bg-dark-1000 rounded text-white text-sm px-2 py-1"
+                  >
+                    {ingrediente}
+                  </p>
+                ))}
             </div>
           </div>
           <div
@@ -64,7 +83,7 @@ function PratoDetalhes() {
             <Counter />
             <div className="max-w-[200px]">
               {/* Finalizar o botão */}
-              <Button title="Incluir - R$ 25,00" />
+              <Button title={`Incluir - R$ ${prato.preco}`} />
             </div>
           </div>
         </div>
