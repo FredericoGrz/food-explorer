0f83871156ceb43b2185272a34e364cafd635992
FredericoGrz <frederico.graz@gmail.com>
2024-01-22 10:53:47 -0300

    Componente altera layout se usuário for admin

--- src/Components/FoodCard/index.jsx
+++ src/Components/FoodCard/index.jsx
@@ -1,16 +1,28 @@
 import { FaRegHeart } from "react-icons/fa";
+import { MdModeEdit } from "react-icons/md";
 import { Counter } from "../Counter";
+import { Image } from "../Image";
+import { useAuth } from "../../hooks/auth";
 import Proptypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export function FoodCard({ img, nome, descricao, preco }) {
+export function FoodCard({ id, img, nome, descricao, preco }) {
+  const { user } = useAuth();
+  const isAdmin = user.isAdmin;
+
   return (
     <div className="min-w-[208px] lg:min-w-[288px] h-72 lg:h-[440px] p-6 bg-dark-200 border border-dark-300 rounded-lg relative flex flex-col gap-3 justify-center items-center">
-      <FaRegHeart className="text-white text-3xl absolute top-4 right-4 cursor-pointer" />
-      <img
-        src={img}
-        alt={`imagem de um(a) ${nome}`}
-        className="w-24 lg:w-44 h-24 lg:h-44"
-      />
+      {isAdmin ? (
+        <Link to={`/prato/${id}`}>
+          <MdModeEdit className="text-white text-3xl absolute top-4 right-4 cursor-pointer z-30 hover:scale-150 transition-transform" />
+        </Link>
+      ) : (
+        <FaRegHeart className="text-white text-3xl absolute top-4 right-4 cursor-pointer z-30 hover:scale-150 transition-transform" />
+      )}
+
+      <div className="w-24 lg:w-44 h-24 lg:h-44">
+        <Image imgUrl={img} />
+      </div>
       <p className="text-white font-semibold lg:text-xl xl:text-2xl">{nome}</p>
 
       <p className="hidden lg:line-clamp-1 lg:h-5 xl:line-clamp-2 xl:h-9 text-xs text-light-400 text-center">
@@ -18,17 +30,20 @@ export function FoodCard({ img, nome, descricao, preco }) {
       </p>
 
       <p className="text-cake-200 text-lg lg:text-xl ">{`R$ ${preco}`}</p>
-      <div className="w-full flex flex-col lg:flex-row lg:mt-2 gap-4 justify-center items-center z-40">
-        <Counter />
-        <button className="text-white font-semibold w-full px-6 py-2 bg-tomato-100 rounded hover:bg-tomato-200">
-          Incluir
-        </button>
-      </div>
+      {!isAdmin && (
+        <div className="w-full flex flex-col lg:flex-row lg:mt-2 gap-4 justify-center items-center z-40">
+          <Counter />
+          <button className="text-white font-semibold w-full px-6 py-2 bg-tomato-100 rounded hover:bg-tomato-200">
+            Incluir
+          </button>
+        </div>
+      )}
     </div>
   );
 }
 
 FoodCard.propTypes = {
+  id: Proptypes.string,
   img: Proptypes.string.isRequired,
   nome: Proptypes.string.isRequired,
   descricao: Proptypes.string.isRequired,
